ac3012e90ed2a2d5251d3d372b948bb30219cdd4
Dasun Wickramasooriya <acc.dasunw@gmail.com>
2025-02-21 16:59:12 +0530

    feat: Add Notifications & UIs

--- src/main/java/com/dasunhq/websockets/controller/NotificationController.java
+++ src/main/java/com/dasunhq/websockets/controller/NotificationController.java
@@ -1,4 +1,17 @@
 package com.dasunhq.websockets.controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class NotificationController {
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/notifications")
+    public String sendMessage(@Payload String message) {
+        System.out.println("Received message: " + message);
+        return message;
+    }
 }
